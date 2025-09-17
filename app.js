da3ee2bc302c8f17a9cd90a57e0c2ce71b1ec21a
elifxo <eabayirli@gmail.com>
2025-09-17 10:20:27 +0100

    Initialize React app with main App component

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 # Boilerplate here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Main App Component
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to React!</h1>
+      <p>This is your boilerplate app.js file.</p>
+    </div>
+  );
+};
+
+// Render the App Component
+ReactDOM.render(<App />, document.getElementById('root'));
+
+export default App;
